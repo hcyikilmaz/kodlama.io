09fd24bc45da775c944ffe3481747c944657a3d6
Ceren Yıkılmaz <cerenyikilmaz@hotmail.com>
2021-01-31 15:37:00 +0300

    Proje dosyası ekle.

---
+++ kodlama.io.odev3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.customerId = 1024589874;
+            customer1.customerName = "Ceren";
+            customer1.customerSurname = "Yıkılmaz";
+            customer1.customerAccountNumber = 8546897458;
+
+            customerManager customerManager = new customerManager();
+            customerManager.Add(customer1);
+
+            customerManager.ChangeAccountNumber(customer1);
+            
+        }
+    }
+}
---
+++ kodlama.io.odev3/customerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class customerManager
+    {
+        public void Add(Customer customer) 
+        {
+
+            Console.WriteLine("Yeni bir müşteri eklendi: " + "Adı: " + customer.customerName);
+        }
+
+        public void ChangeAccountNumber(Customer customer)
+        {
+            Console.WriteLine("Eski Hesap Numarası: " + customer.customerAccountNumber);
+            customer.customerAccountNumber = 5897485468;
+            Console.WriteLine("Müşterinin hesap numarası değiştirildi: " + customer.customerAccountNumber);
+        }
+    }
+}
